6be7c78d89bbc5dd638b7e39325b492f1e2ff435
RobinJahn <robin@kamerapilot.com>
2023-11-03 11:50:26 +0100

    Initial commit

---
+++ src/main/java/com/example/itutor/ITutorApplication.java
@@ -0,0 +1,13 @@
+package com.example.itutor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ITutorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ITutorApplication.class, args);
+    }
+
+}
