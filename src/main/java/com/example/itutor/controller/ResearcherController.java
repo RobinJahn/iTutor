c705423a140675bea9ac67caa0b8d694c5076686
paulf <paul.frischeisen@web.de>
2023-11-28 15:44:09 +0100

    Initial commit ResearcherController

---
+++ src/main/java/com/example/itutor/controller/ResearcherController.java
@@ -0,0 +1,89 @@
+package com.example.itutor.controller;
+
+import com.example.itutor.domain.Researcher;
+import com.example.itutor.domain.User;
+import com.example.itutor.service.UserServiceI;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+public class ResearcherController {
+
+    private UserServiceI researcherService;
+
+    public ResearcherController(UserServiceI researcherService) {
+        super(); //???
+        this.researcherService = researcherService;
+    }
+
+    @RequestMapping(value = "/researchers/signup", method = RequestMethod.GET)
+    public String showResearcherSignup(HttpServletRequest request, Model model) {
+
+        Researcher researcher = new Researcher();
+        model.addAttribute("user", researcher);
+
+        model.addAttribute("link", "/researchers/add/process");
+
+        return "signup";
+    }
+
+    @RequestMapping(value = "/researchers/add/process")
+    public String addResearcher(@ModelAttribute @Valid Researcher researcherRequest,
+                                BindingResult result,
+                                RedirectAttributes attr){
+
+        if (result.hasErrors()){
+            System.out.println(result.getErrorCount());
+            System.out.println(result.getAllErrors());
+            return "signup";
+        }
+
+        //TODO: save researcher to database
+        User createdResearcher = researcherService.saveResearcher(researcherRequest);
+        System.out.println(createdResearcher);
+
+        attr.addFlashAttribute("success", "Researcher added!");
+        return "redirect:/home";
+    }
+
+    @RequestMapping(value = "/researchers/edit/{researcherId}", method = RequestMethod.GET)
+    public String editResearcherForm(@PathVariable Long researcherId, Model model) {
+        // get student from database by id
+        User researcher = researcherService.getResearcherById(researcherId);
+
+        if (researcher != null) {
+            // Add the student to the model to pre-populate the form on the page
+            model.addAttribute("researcher", researcher);
+            return "editResearcher";
+        } else {
+            // if student was not found -> redirect to another page
+            return "errorPage";
+        }
+    }
+
+    @RequestMapping(value = "/researchers/edit/process", method = RequestMethod.POST)
+    public String editResearcher(@ModelAttribute @Valid Researcher researcherRequest,
+                              BindingResult result,
+                              RedirectAttributes attr) {
+
+        if (result.hasErrors()) {
+            System.out.println(result.getErrorCount());
+            System.out.println(result.getAllErrors());
+            return "editResearcher"; // if there are any errors -> back to edit form
+        }
+
+        //TODO: Update the student details in the database based on the edited data in studentRequest
+
+        attr.addFlashAttribute("success", "Researcher updated!");
+        return "redirect:/home";
+    }
+
+}
