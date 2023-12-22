e33d61e2e53bdc9a5afa43adbda79a1a8351216d
paulf <paul.frischeisen@web.de>
2023-12-22 13:31:14 +0100

    Set up a ChatController and basic logic for Chat Messages; Also set up an EventListener for the Web Socket

---
+++ src/main/java/com/example/itutor/controller/ChatController.java
@@ -0,0 +1,19 @@
+package com.example.itutor.controller;
+
+import com.example.itutor.domain.chat.ChatMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatController {
+
+    @MessageMapping("/chat.sendMessage")
+    @SendTo("/topic/public")
+    public ChatMessage sendMessage(
+            @Payload ChatMessage chatMessage
+    ) {
+        return chatMessage;
+    }
+}
