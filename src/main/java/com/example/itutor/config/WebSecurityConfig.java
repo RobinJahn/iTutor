f4ce3a77f5f53bf3c58c7ea5e0b89588fbac95a8
RobinJahn <robin@kamerapilot.com>
2024-01-23 16:22:24 +0100

    Fixed browser login request

--- src/main/java/com/example/itutor/config/WebSecurityConfig.java
+++ src/main/java/com/example/itutor/config/WebSecurityConfig.java
@@ -4,10 +4,13 @@ import com.example.itutor.service.impl.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -17,6 +20,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.OrRequestMatcher;
 import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -43,6 +47,20 @@ public class WebSecurityConfig {
     }
 
     @Bean
+    @Order(1)
+    public SecurityFilterChain apiSecurityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .securityMatcher(new AntPathRequestMatcher("/api/**"))
+                .authorizeHttpRequests((requests) -> requests
+                        .anyRequest().authenticated()
+                )
+                .csrf().disable()
+                .httpBasic(withDefaults());
+
+        return http.build();
+    }
+
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((requests) -> requests
@@ -58,12 +76,14 @@ public class WebSecurityConfig {
                         .requestMatchers(new MvcRequestMatcher(handlerMappingIntrospector, "/researchers/add/process")).permitAll()
 
                         .requestMatchers(new MvcRequestMatcher(handlerMappingIntrospector, "/students/motivation")).hasAuthority("STUDENT")
-                        .requestMatchers(new MvcRequestMatcher(handlerMappingIntrospector,"/experts/guideline")).hasAuthority("EXPERT")
-                        .requestMatchers(new MvcRequestMatcher(handlerMappingIntrospector,"/statistics")).hasAuthority("RESEARCHER")
+                        .requestMatchers(new MvcRequestMatcher(handlerMappingIntrospector, "/experts/guideline")).hasAuthority("EXPERT")
+                        .requestMatchers(new MvcRequestMatcher(handlerMappingIntrospector, "/statistics")).hasAuthority("RESEARCHER")
 
-                        .requestMatchers(new MvcRequestMatcher(handlerMappingIntrospector,"/css/**")).permitAll()
-                        .requestMatchers(new MvcRequestMatcher(handlerMappingIntrospector,"/js/**")).permitAll()
-                        .requestMatchers(new MvcRequestMatcher(handlerMappingIntrospector,"/image/**")).permitAll()
+                        .requestMatchers(new MvcRequestMatcher(handlerMappingIntrospector, "/css/**")).permitAll()
+                        .requestMatchers(new MvcRequestMatcher(handlerMappingIntrospector, "/js/**")).permitAll()
+                        .requestMatchers(new MvcRequestMatcher(handlerMappingIntrospector, "/image/**")).permitAll()
+
+
                         .anyRequest().authenticated()
                 )
                 .formLogin((form) -> form
@@ -71,9 +91,7 @@ public class WebSecurityConfig {
                         .permitAll()
                 )
                 .logout(LogoutConfigurer::permitAll)
-                .csrf().disable()
-                .httpBasic(withDefaults());
-
+        ;
 
         return http.build();
     }
